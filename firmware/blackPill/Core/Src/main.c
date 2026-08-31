22b637405a6dac1bdbeeb649cf9881079be25250
minawashere <minasalama2580@gmail.com>
2026-09-01 01:45:13 +0300

    Black Pill configuration for IR

--- firmware/blackPill/Core/Src/main.c
+++ firmware/blackPill/Core/Src/main.c
@@ -362,7 +362,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 83;
+  htim4.Init.Prescaler = 8300;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 1000;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
