cb9cad2a55243ad98813db6a3244a926bb616204
minawashere <minasalama2580@gmail.com>
2026-09-01 01:36:36 +0300

    Black Pill configuration for IR

--- firmware/blackPill/Core/Src/main.c
+++ firmware/blackPill/Core/Src/main.c
@@ -321,13 +321,11 @@ static void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-  sConfigOC.OCMode = TIM_OCMODE_PWM2;
   sConfigOC.Pulse = 0;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
   }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
